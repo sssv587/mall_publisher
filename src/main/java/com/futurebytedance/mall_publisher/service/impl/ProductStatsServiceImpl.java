5436dccba743aca4713766bb70902378571ec361
sssv587 <1092663295@qq.com>
2022-02-07 00:48:10 +0800

    ProductStatsServiceImpl

--- src/main/java/com/futurebytedance/mall_publisher/service/impl/ProductStatsServiceImpl.java
+++ src/main/java/com/futurebytedance/mall_publisher/service/impl/ProductStatsServiceImpl.java
@@ -1,11 +1,13 @@
 package com.futurebytedance.mall_publisher.service.impl;
 
+import com.futurebytedance.mall_publisher.bean.ProductStats;
 import com.futurebytedance.mall_publisher.mapper.ProductStatsMapper;
 import com.futurebytedance.mall_publisher.service.ProductStatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author yuhang.sun
@@ -22,5 +24,20 @@ public class ProductStatsServiceImpl implements ProductStatsService {
     @Override
     public BigDecimal getGMV(int date) {
         return productStatsMapper.getGMV(date);
+    }
+
+    @Override
+    public List<ProductStats> getProductStatsByTrademark(int date, int limit) {
+        return productStatsMapper.getProductStatsByTrademark(date, limit);
+    }
+
+    @Override
+    public List<ProductStats> getProductStatsByCategory3(int date, int limit) {
+        return productStatsMapper.getProductStatsByCategory3(date, limit);
+    }
+
+    @Override
+    public List<ProductStats> getProductStatsGroupBySpu(int date, int limit) {
+        return productStatsMapper.getProductStatsGroupBySpu(date, limit);
     }
 }
