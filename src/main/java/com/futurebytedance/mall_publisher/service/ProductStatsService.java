bf1c1640a2465f92fb7e5632ffcf3004c9daad6a
sssv587 <1092663295@qq.com>
2022-02-07 00:48:15 +0800

    ProductStatsService

--- src/main/java/com/futurebytedance/mall_publisher/service/ProductStatsService.java
+++ src/main/java/com/futurebytedance/mall_publisher/service/ProductStatsService.java
@@ -1,6 +1,10 @@
 package com.futurebytedance.mall_publisher.service;
 
+import com.futurebytedance.mall_publisher.bean.ProductStats;
+import org.apache.ibatis.annotations.Param;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author yuhang.sun
@@ -11,4 +15,13 @@ import java.math.BigDecimal;
 public interface ProductStatsService {
     //获取某一天交易总额
     BigDecimal getGMV(int date);
+
+    //获取某一天不同品牌的交易额
+    List<ProductStats> getProductStatsByTrademark(int date, int limit);
+
+    //获取某一天不同品类的交易额
+    List<ProductStats> getProductStatsByCategory3(int date, int limit);
+
+    //统计某天不同 SPU 商品交易额排名
+    List<ProductStats> getProductStatsGroupBySpu(int date, int limit);
 }
